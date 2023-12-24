c85d1dbd235a7abea70d0b3eeaa2a1d5a692d21d
Ren Sato <miseria.ren@gmail.com>
2023-12-24 18:03:46 +0900

    Add time Listener.py and package

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,12 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16, String
 
-def cb(msg):
-    global node
+def count_callback(msg):
     node.get_logger().info("Listen: %d" % msg.data)
+
+def time_callback(msg):
+    node.get_logger().info("現在時刻: %s" % msg.data)
 
 rclpy.init()
 node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 10)
+sub_count = node.create_subscription(Int16, "countup", count_callback, 10)
+sub_time = node.create_subscription(String, "current_time", time_callback, 10)
 rclpy.spin(node)
+
