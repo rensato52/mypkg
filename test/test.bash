719edaf6db0ae010a196c40b0d05f45679a96981
Ren Sato <miseria.ren@gmail.com>
2023-12-27 16:40:37 +0900

    add current_time test

--- test/test.bash
+++ test/test.bash
@@ -7,8 +7,14 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
+
 source $dir/.bashrc
+
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Listen: 10'
+
+cat /tmp/mypkg.log |
+grep '現在時刻:'
+
